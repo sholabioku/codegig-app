34d43c5e115de13c9f9c3e46d6da9e3943f227c5
Shola Bioku <bilushonline@gmail.com>
2020-07-29 16:20:28 +0100

    Add GET /gigs/add endpoint

--- routes/gigs.js
+++ routes/gigs.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../config/database');
 const Gig = require('../models/Gig');
 
+// Get All Available Gigs
 router.get('/', (req, res) =>
   Gig.findAll()
     .then((gigs) => {
@@ -11,5 +12,24 @@ router.get('/', (req, res) =>
     })
     .catch((err) => console.log(err))
 );
+
+// Add a Gig
+router.get('/add', (req, res) => {
+  const data = {
+    title: 'Looking for a React Developer',
+    technologies: 'react,javascript,html,css',
+    budget: '$3000',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    contact_email: 'shola@gmail.com',
+  };
+
+  let { title, technologies, budget, description, contact_email } = data;
+
+  // Insert data
+  Gig.create({ title, technologies, budget, description, contact_email })
+    .then((gig) => res.redirect('/gigs'))
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
