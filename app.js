ea02c7ffd5c5364ffb1ed7279ddcb74c78b18668
Shola Bioku <bilushonline@gmail.com>
2020-07-29 10:04:18 +0100

    Connect to sequelize database

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const Sequelize = require('sequelize');
+
+// Option 1: Passing parameters separately
+const db = new Sequelize('codegig', 'bilush', '5346', {
+  host: 'localhost',
+  dialect: 'postgres',
+});
+
+db.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch((err) => console.log(`Error: ${err}`));
+
 const app = express();
 
 app.get('/', (req, res) => res.send('INDEX'));
