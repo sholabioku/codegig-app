acfd43895690c559d501ac999b4a9937fdf0ebad
Shola Bioku <bilushonline@gmail.com>
2020-07-30 02:49:05 +0100

    Render index page as landing page

--- app.js
+++ app.js
@@ -25,7 +25,9 @@ app.engine(
 );
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => res.send('INDEX'));
+// Index route
+
+app.get('/', (req, res) => res.render('index', { layout: 'landing' }));
 
 // Set Static Folder
 app.use(express.static(path.join((__dirname, 'public'))));
