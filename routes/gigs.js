a0ea3733a902833fe53359650bfc653bfd2108f3
Shola Bioku <bilushonline@gmail.com>
2020-07-30 03:03:56 +0100

    routes/gigs.jsPull data from req.body

--- routes/gigs.js
+++ routes/gigs.js
@@ -17,16 +17,7 @@ router.get('/add', (req, res) => res.render('add'));
 
 // Add a Gig
 router.post('/add', (req, res) => {
-  const data = {
-    title: 'Simple Wordpress Website',
-    technologies: 'wordpress,php,html,css',
-    budget: '$1000',
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    contact_email: 'bilush@gmail.com',
-  };
-
-  let { title, technologies, budget, description, contact_email } = data;
+  let { title, technologies, budget, description, contact_email } = req.body;
 
   // Insert data
   Gig.create({ title, technologies, budget, description, contact_email })
